560ddcc21c7c7a5e7bbd5371644536737fbd9388
Sina Negarandeh <73743663+sina-negarandeh@users.noreply.github.com>
2021-04-18 03:37:35 -0700

    Create test_bench.v

---
+++ src/test_bench.v
@@ -0,0 +1,18 @@
+module Test_Bench ();
+ 
+  integer i;
+  reg clk, rst;
+ 
+  ARM arm (clk, rst);
+
+  initial begin
+    rst = 1'b0;
+    #35 rst = 1'b1;
+    #35 rst = 1'b0;
+    for (i = 0; i < 10; i=i+1) begin
+      #100 clk = 1'b0;
+      #100 clk = 1'b1;
+    end
+  end
+
+endmodule
