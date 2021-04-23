4d79fe9d4466b6919ebc028cc0a3ac139f5845e7
Sina Negarandeh <73743663+sina-negarandeh@users.noreply.github.com>
2021-04-23 01:05:10 -0700

    Add input and output ports for Instruction Decode Stage and Reg

--- src/id_stage.v
+++ src/id_stage.v
@@ -1,10 +1,41 @@
 module ID_Stage (
   input         clk,
   input         rst,
-  input [31:0]  pc_in,
-  output [31:0] pc
-  );
   
-  assign pc = pc_in;
+  // From Instruction Fetch Stage
+  input [31:0]  pc_in,
+  input [31:0]  instruction,
+  
+  // From Hazard Detection Unit
+  input         hazard,
+  
+  // From Status Register
+  input [3:0]   sr,
+  
+  // From Write Back Stage
+  input [3:0]   wb_dest,
+  input [31:0]  wb_value,
+  input         wb_wb_en,
+  
+  output        wb_en,
+  output        mem_r_en,
+  output        mem_w_en,
+  output [3:0]  exe_cmd,
+  output        b,
+  output        s,
+  output        pc,
+  output [31:0] value_rn,
+  output [31:0] value_rm,
+  output [11:0] shift_operand,
+  output imm,
+  output [23:0] imm_signed_24,
+  output [3:0]  dest,
+  
+  // To Hazard Detection Unit
+  
+  output        two_src,
+  output [3:0]  src_1,
+  output [3:0]  src_2
+  );
 
 endmodule
--- src/id_stage_reg.v
+++ src/id_stage_reg.v
@@ -1,16 +1,38 @@
 module ID_Stage_Reg (
-  input             clk,
-  input             rst,
-  output [31:0]     pc_in,
-  output reg [31:0] pc
+  input              clk,
+  input              rst,
+  
+  // From Instruction Decode Stage
+  input              wb_en_in,
+  input              mem_r_en_in,
+  input              mem_w_en_in,
+  input [3:0]        exe_cmd_in,
+  input              b_in,
+  input              s_in,
+  input              pc_in,
+  input [31:0]       value_rn_in,
+  input [31:0]       value_rm_in,
+  input [11:0]       shift_operand_in,
+  input              imm_in,
+  input [23:0]       imm_signed_24_in,
+  input [3:0]        dest_in,
+  
+  // From Executaion Stage
+  input              flush,
+  
+  output             wb_en,
+  output             mem_r_en,
+  output             mem_w_en,
+  output [3:0]       exe_cmd,
+  output             b,
+  output             s,
+  output             pc,
+  output [31:0]      value_rn,
+  output [31:0]      value_rm,
+  output [11:0]      shift_operand,
+  output             imm,
+  output [23:0]      imm_signed_24,
+  output [3:0]       dest 
   );
-
-  always @ (posedge clk, posedge rst) begin
-    if (rst) begin
-      pc <= 32'b00000000000000000000000000000000;
-    end else begin
-      pc <= pc_in;
-    end
-  end
 
 endmodule
