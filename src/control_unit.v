4b6347d01ffa3be1e101a9f95857a1aef3fd1a31
Sina Negarandeh <73743663+sina-negarandeh@users.noreply.github.com>
2021-04-23 01:02:06 -0700

    Fix syntax error

--- src/control_unit.v
+++ src/control_unit.v
@@ -5,7 +5,7 @@ module ControlUnit (
   
   output [3:0]  exe_cmd,
   output        mem_r_en, // mem_read
-  output        mem_w_en  // mem_w_en
+  output        mem_w_en,  // mem_w_en
   output        wb_en,
   output        s,
   output        b
