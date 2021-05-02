599c73651c3a0f8cd3a1a182b34695507ecf6833
Sina Negarandeh <73743663+sina-negarandeh@users.noreply.github.com>
2021-05-02 01:48:02 -0700

    Complete Instruction Decode Stage Reg

--- src/id_stage_reg.v
+++ src/id_stage_reg.v
@@ -20,19 +20,51 @@ module ID_Stage_Reg (
   // From Executaion Stage
   input              flush,
   
-  output             wb_en,
-  output             mem_r_en,
-  output             mem_w_en,
-  output [3:0]       exe_cmd,
-  output             b,
-  output             s,
-  output             pc,
-  output [31:0]      value_rn,
-  output [31:0]      value_rm,
-  output [11:0]      shift_operand,
-  output             imm,
-  output [23:0]      imm_signed_24,
-  output [3:0]       dest 
+  output reg        wb_en,
+  output reg        mem_r_en,
+  output reg        mem_w_en,
+  output reg [3:0]  exe_cmd,
+  output reg        b,
+  output reg        s,
+  output reg [31:0] pc,
+  output reg [31:0] value_rn,
+  output reg [31:0] value_rm,
+  output reg [11:0] shift_operand,
+  output reg        imm,
+  output reg [23:0] imm_signed_24,
+  output reg [3:0]  dest 
   );
+  
+  always @ (posedge clk, posedge rst) begin
+    if (rst) begin
+      wb_en <= 1'b0;
+      mem_r_en <= 1'b0;
+      mem_w_en <= 1'b0;
+      exe_cmd <= 4'b0000;
+      b <= 1'b0;
+      s <= 1'b0;
+      pc <= 32'b00000000000000000000000000000000;
+      value_rn <= 32'b00000000000000000000000000000000;
+      value_rm <= 32'b00000000000000000000000000000000;
+      shift_operand <= 12'b000000000000;
+      imm <= 1'b0;
+      imm_signed_24 <= 24'b000000000000000000000000;
+      dest <= 4'b0000;
+    end else begin
+      wb_en <= wb_en_in;
+      mem_r_en <= mem_r_en_in;
+      mem_w_en <= mem_w_en_in;
+      exe_cmd <= exe_cmd_in;
+      b <= b_in;
+      s <= s_in;
+      pc <= pc_in;
+      value_rn <= value_rn_in;
+      value_rm <= value_rm_in;
+      shift_operand <= shift_operand_in;
+      imm <= imm_in;
+      imm_signed_24 <= imm_signed_24_in;
+      dest <= dest_in;
+    end
+  end
 
 endmodule
