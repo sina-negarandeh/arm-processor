abdc7faafa1b826fbb89b1428de66ed7c02b5219
Sina Negarandeh <73743663+sina-negarandeh@users.noreply.github.com>
2021-05-08 00:56:19 -0700

    Complete Write Back Stage

--- src/wb_stage.v
+++ src/wb_stage.v
@@ -1,10 +1,21 @@
 module WB_Stage (
   input         clk,
   input         rst,
-  input [31:0]  pc_in,
-  output [31:0] pc
+
+  input [31:0]  alu_result,
+  input [31:0]  mem_result,
+  input         mem_r_en,
+  input [3:0]   wb_dest_in,
+  input         wb_wb_en_in,
+  
+  
+  output [3:0]  wb_dest,
+  output [31:0] wb_value,
+  output        wb_wb_en
   );
 
-  assign pc = pc_in;
+  assign wb_dest = wb_dest_in;
+  assign wb_value = mem_r_en ? mem_result : alu_result;
+  assign wb_wb_en = wb_wb_en_in;
 
 endmodule
