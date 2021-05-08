be26a9dafb79e83ce2ccb5a85f2995a2d4098c51
Sina Negarandeh <73743663+sina-negarandeh@users.noreply.github.com>
2021-05-08 00:55:55 -0700

    Complete Memory Stage Reg

--- src/mem_stage_reg.v
+++ src/mem_stage_reg.v
@@ -1,15 +1,33 @@
 module MEM_Stage_Reg (
   input             clk,
   input             rst,
-  output [31:0]     pc_in,
-  output reg [31:0] pc
+
+  input             wb_en_in,
+  input             mem_r_en_in,
+  input [31:0]      alu_result_in,
+  input [31:0]      data_memory_out_in,
+  input [3:0]       dest_in,
+
+  output reg        wb_en,
+  output reg        mem_r_en,
+  output reg [31:0] alu_result,
+  output reg [31:0] data_memory_out,
+  output reg [3:0]  dest
   );
 
   always @ (posedge clk, posedge rst) begin
     if (rst) begin
-      pc <= 32'b00000000000000000000000000000000;
+      wb_en <= 1'b0;
+      mem_r_en <= 1'b0;
+      alu_result <= 32'b00000000000000000000000000000000;
+      data_memory_out <= 32'b00000000000000000000000000000000;
+      dest <= 4'b0000;
     end else begin
-      pc <= pc_in;
+      wb_en <= wb_en_in;
+      mem_r_en <= mem_r_en_in;
+      alu_result <= alu_result_in;
+      data_memory_out <= data_memory_out_in;
+      dest <= dest_in;
     end
   end
 
