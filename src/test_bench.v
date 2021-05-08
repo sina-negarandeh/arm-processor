9a5bcb8943abbb10f1f8e0a56184da5fdd58cee7
Sina Negarandeh <73743663+sina-negarandeh@users.noreply.github.com>
2021-05-08 00:59:18 -0700

    Update test_bench.v

--- src/test_bench.v
+++ src/test_bench.v
@@ -9,7 +9,7 @@ module Test_Bench ();
     rst = 1'b0;
     #35 rst = 1'b1;
     #35 rst = 1'b0;
-    for (i = 0; i < 20; i=i+1) begin
+    for (i = 0; i < 40; i=i+1) begin
       #100 clk = 1'b0;
       #100 clk = 1'b1;
     end
