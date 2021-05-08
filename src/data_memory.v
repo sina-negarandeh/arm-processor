0cb0d879e5c93fe8c4a9582f6d8f92d13e0d355c
Sina Negarandeh <73743663+sina-negarandeh@users.noreply.github.com>
2021-05-08 03:45:49 -0700

    Change Memory block size

--- src/data_memory.v
+++ src/data_memory.v
@@ -10,7 +10,7 @@ module DataMemory (
   output reg [31:0] data_memory_out
   );
   
-  reg [7:0] memory[0:255];
+  reg [31:0] memory[0:64];
   
   wire [31:0] aligned_address;
   assign aligned_address = ((address - 1024) >> 2);
@@ -24,16 +24,20 @@ module DataMemory (
   */
   always @ (*) begin
     if (mem_r_en) begin
-      data_memory_out = {memory[{{aligned_address}, {2'b00}}], memory[{{aligned_address}, {2'b01}}], memory[{{aligned_address}, {2'b10}}], memory[{{aligned_address}, {2'b11}}]};
+      data_memory_out = memory[aligned_address];
+      //data_memory_out = {memory[{{aligned_address}, {2'b00}}], memory[{{aligned_address}, {2'b01}}], memory[{{aligned_address}, {2'b10}}], memory[{{aligned_address}, {2'b11}}]};
     end
   end
   
   always @ (posedge clk, posedge rst) begin
     if (mem_w_en) begin
-      memory[aligned_address]     <= data[31:24];
-      memory[aligned_address + 1] <= data[23:16];
-      memory[aligned_address + 2] <= data[15:8];
-      memory[aligned_address + 3] <= data[7:0];
+      memory[aligned_address] <= data;
+      /*
+      memory[aligned_address + 2'b00] <= data[31:24];
+      memory[aligned_address + 2'b01] <= data[23:16];
+      memory[aligned_address + 2'b10] <= data[15:8];
+      memory[aligned_address + 2'b11] <= data[7:0];
+      */
     end
   end
 
