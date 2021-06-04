0ea2b5272c6ec356ed89b49032e16ad6289e96f0
Sina Negarandeh <73743663+sina-negarandeh@users.noreply.github.com>
2021-06-04 04:07:46 -0700

    Add sram support to Memory Stage

--- src/mem_stage.v
+++ src/mem_stage.v
@@ -14,15 +14,21 @@ module MEM_Stage (
   output        mem_w_en,
   output [31:0] alu_result_out,
   output [31:0] data_memory_out,
-  output [3:0]  dest
+  output [3:0]  dest,
+  
+  inout [31:0]  sram_dq,
+  output        sram_w_en,   
+  output [16:0] sram_address,
+  output        ready
   );
 
   assign wb_en = wb_en_in;
   assign mem_r_en = mem_r_en_in;
   assign mem_w_en = mem_w_en_in;
-  assign alu_result_out = alu_result; 
+  assign alu_result_out = alu_result;
   
-  DataMemory data_memory (clk, rst, mem_r_en_in, mem_w_en_in, alu_result, rm_val, data_memory_out);
+  //DataMemory data_memory (clk, rst, mem_r_en_in, mem_w_en_in, alu_result, rm_val, data_memory_out);
+  SRAM_Controller sram_controller (clk, rst, mem_r_en_in, mem_w_en_in, alu_result, rm_val, sram_dq, sram_w_en, data_memory_out, sram_address, ready);
   
   assign dest = dest_in;
 
